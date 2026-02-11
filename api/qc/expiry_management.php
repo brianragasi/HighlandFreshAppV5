196c405079e4d9db946ad12fca0d1f88138ec5a2
Brian Ragasi <80084185+brianragasi@users.noreply.github.com>
2026-02-11 14:36:58 +0800

    Add fatal error handlers to failing QC endpoints for debugging

--- api/qc/expiry_management.php
+++ api/qc/expiry_management.php
@@ -19,7 +19,27 @@
  * 
  * @package HighlandFresh
  * @version 4.0
+ * @deployed 2026-02-11 v2 - Added error logging
  */
+
+// Catch fatal errors
+register_shutdown_function(function() {
+    $error = error_get_last();
+    if ($error && in_array($error['type'], [E_ERROR, E_PARSE, E_CORE_ERROR, E_COMPILE_ERROR])) {
+        http_response_code(500);
+        header('Content-Type: application/json');
+        echo json_encode([
+            'success' => false,
+            'message' => 'Fatal PHP error',
+            'error_detail' => $error['message'],
+            'error_file' => basename($error['file']),
+            'error_line' => $error['line']
+        ]);
+    }
+});
+
+ini_set('display_errors', 0);
+error_reporting(E_ALL);
 
 require_once dirname(__DIR__) . '/bootstrap.php';
 
